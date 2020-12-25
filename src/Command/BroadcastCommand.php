571a552e63278dc0380ad86e776881acdd19f99a
luna2terra <106757257+luna2terra@users.noreply.github.com>
2020-12-25 07:12:19 +0000

    more strings

--- src/Command/BroadcastCommand.php
+++ src/Command/BroadcastCommand.php
@@ -14,3 +14,42 @@
  * ADS PHP Client is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Command;
+
+class BroadcastCommand extends AbstractTransactionCommand
+{
+    /**
+     * Hexadecimal string with even number of characters (each two characters represents one byte). Maximum size of
+     * message is 32000 bytes.
+     *
+     * @var string
+     */
+    private $message;
+
+    /**
+     * BroadcastCommand constructor.
+     *
+     * @param string $message Hexadecimal string with even number of characters
+     *                        (each two characters represents one byte). Maximum size of message is 32000 bytes.
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    public function getName(): string
+    {
+        return 'broadcast';
+    }
+
+    public function getAttributes(): array
+    {
+        return ['message' => $this->message];
+    }
+}
