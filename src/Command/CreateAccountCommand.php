7729fd2873159017a2365636ecd784ee23c0b059
luna2terra <106757257+luna2terra@users.noreply.github.com>
2020-12-29 18:02:11 +0000

    Vendor

--- src/Command/CreateAccountCommand.php
+++ src/Command/CreateAccountCommand.php
@@ -15,4 +15,13 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General
+ * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Command;
+
+class CreateAccountCommand extends AbstractTransactionCommand
+{
+    /**
+     * 
