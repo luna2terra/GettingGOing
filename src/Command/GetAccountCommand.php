95a1b3de8eb397408ecd82073a24f9ff55797330
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-01-07 16:59:28 +0000

    Fixing references

--- src/Command/GetAccountCommand.php
+++ src/Command/GetAccountCommand.php
@@ -15,3 +15,12 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
+ * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Command;
+
+class GetAccountCommand extends AbstractCommand
+{
+   
