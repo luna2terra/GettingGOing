1fd3455ddb8a2decd080c1a01dd1f3903204a003
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-01-08 18:27:07 +0000

    README: typo

--- src/Command/GetAccountsCommand.php
+++ src/Command/GetAccountsCommand.php
@@ -16,3 +16,13 @@
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Command;
+
+class GetAccountsCommand extends AbstractCommand
+{
+    /**
+     * @var null|string
+     */
