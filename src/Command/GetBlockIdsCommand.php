133d9eeaa09cfdc36dd2ba947c52868b00ef7b23
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-01-27 14:19:15 +0000

    Update Xcode project.

--- src/Command/GetBlockIdsCommand.php
+++ src/Command/GetBlockIdsCommand.php
@@ -16,4 +16,18 @@
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with AD
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Command;
+
+class GetBlockIdsCommand extends AbstractCommand
+{
+    /**
+     *
+     * @var null|string
+     */
+    private $blockIdFrom;
+    /**
+     *
+  
