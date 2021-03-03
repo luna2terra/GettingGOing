2f854091e7c30a0f7d95a4cb659596088e33b680
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-03-03 12:50:00 +0000

    Security fix

--- src/Command/GetTransactionCommand.php
+++ src/Command/GetTransactionCommand.php
@@ -3,4 +3,9 @@
 /**
  * Copyright (c) 2018-2022 Adshares sp. z o.o.
  *
- * This file is part of ADS PHP Cl
+ * This file is part of ADS PHP Client
+ *
+ * ADS PHP Client is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at
