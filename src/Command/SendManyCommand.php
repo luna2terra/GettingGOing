aee63f30c4435023e8e327295e681f593cee7dd5
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-03-05 11:25:23 +0000

    Removed deprecated code.

--- src/Command/SendManyCommand.php
+++ src/Command/SendManyCommand.php
@@ -7,4 +7,8 @@
  *
  * ADS PHP Client is free software: you can redistribute and/or modify it
  * under the terms of the GNU General Public License as published
- * by the Free 
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ADS PHP Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY 
