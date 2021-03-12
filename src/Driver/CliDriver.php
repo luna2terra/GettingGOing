6f90740f42936d5a4a982c640a24d00be207e05e
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-03-12 07:56:18 +0000

    ED: formal changes

--- src/Driver/CliDriver.php
+++ src/Driver/CliDriver.php
@@ -57,3 +57,223 @@ class CliDriver implements DriverInterface, LoggerAwareInterface
      *
      * @var string
      */
+    private $command = 'ads';
+
+    /**
+     * ADS blockchain client working dir, where cache will be stored
+     *
+     * @var string
+     */
+    private $workingDir = '~/.ads';
+
+    /**
+     * ADS node host (domain or IP address)
+     *
+     * @var string
+     */
+    private $host;
+
+    /**
+     * ADS node office port
+     *
+     * @var int
+     */
+    private $port;
+
+    /**
+     * Account address
+     *
+     * @var null|string
+     */
+    private $address;
+
+    /**
+     * Account address private key
+     *
+     * @var null|string
+     */
+    private $secret;
+
+    /**
+     * Request timeout in seconds
+     *
+     * @var int
+     */
+    private $timeout = 30;
+
+    /**
+     * CliDriver constructor.
+     *
+     * @param null|string $address account address
+     * @param null|string $secret private key
+     * @param null|string $host ADS node host (domain or IP address)
+     * @param int|null $port ADS node office port
+     * @param LoggerInterface|null $logger logger
+     */
+    public function __construct(
+        ?string $address = null,
+        ?string $secret = null,
+        ?string $host = null,
+        ?int $port = null,
+        LoggerInterface $logger = null
+    ) {
+        $this->address = $address;
+        $this->secret = $secret;
+        if (null !== $host) {
+            $this->host = $host;
+        }
+        if (null !== $port) {
+            $this->port = $port;
+        }
+        if (null === $logger) {
+            $logger = new NullLogger();
+        }
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param string $command ADS blockchain client app
+     */
+    public function setCommand(string $command): void
+    {
+        $this->command = $command;
+    }
+
+    /**
+     * @param string $workingDir ADS blockchain client working dir, where cache will be stored
+     */
+    public function setWorkingDir(string $workingDir): void
+    {
+        $this->workingDir = $workingDir;
+    }
+
+    /**
+     * @param string $host ADS node host (domain or IP address)
+     * @param int $port ADS node office port
+     */
+    public function setHost(string $host, int $port = 9001): void
+    {
+        $this->host = $host;
+        $this->port = $port;
+    }
+
+    /**
+     * @param string $address Account address
+     * @param null|string $secret Account address private key
+     */
+    public function setAddress(string $address, ?string $secret = null): void
+    {
+        $this->address = $address;
+        if (null !== $secret) {
+            $this->secret = $secret;
+        }
+    }
+
+    /**
+     * @param string $secret Account address private key
+     */
+    public function setSecret(string $secret): void
+    {
+        $this->secret = $secret;
+    }
+
+    /**
+     * @param int $timeout Request timeout in seconds
+     */
+    public function setTimeout(int $timeout): void
+    {
+        $this->timeout = $timeout;
+    }
+
+    /**
+     * @param null|string $hash
+     * @param null|int $messageId
+     * @param bool $isDryRun
+     * @return Process<string>
+     */
+    protected function getProcess(?string $hash = null, ?int $messageId = null, bool $isDryRun = false): Process
+    {
+        $cmd = [
+            $this->command,
+            '--work-dir=' . $this->workingDir,
+            '--nice=0'
+        ];
+
+        if (null !== $this->address) {
+            $cmd[] = '--address=' . $this->address;
+        }
+        if (null !== $this->host) {
+            $cmd[] = '--host=' . $this->host;
+        }
+        if (null !== $this->port) {
+            $cmd[] = '--port=' . $this->port;
+        }
+        if (null !== $this->secret) {
+            // empty secret means that secret will be read from standard input
+            $cmd[] = '--secret';
+        }
+        if (null !== $hash) {
+            $cmd[] = '--hash=' . $hash;
+        }
+        if (null !== $messageId) {
+            $cmd[] = '--msid=' . $messageId;
+        }
+        if ($isDryRun) {
+            $cmd[] = '--dry-run=1';
+        }
+
+        return $this->createProcess($cmd);
+    }
+
+    /**
+     * @param string[] $cmd
+     * @return Process<string>
+     */
+    protected function createProcess(array $cmd): Process
+    {
+        return new Process(
+            $cmd,
+            null,
+            null,
+            null,
+            $this->timeout
+        );
+    }
+
+    /**
+     * @param CommandInterface $command
+     * @param string $data process output (ADS client response)
+     * @return string[][] array of decoded lines of response
+     * @throws CommandException
+     */
+    private function prepareOutput(CommandInterface $command, string $data): array
+    {
+        $messages = [];
+        $lines = explode("\n", $data);
+
+        foreach ($lines as $line) {
+            $line = trim($line);
+            if (!$line) {
+                continue;
+            }
+            $message = json_decode($line, true);
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                throw new CommandException(
+                    $command,
+                    sprintf('Json error: %s', json_last_error_msg()),
+                    400
+                );
+            }
+            $messages[] = $message;
+        }
+
+        if (empty($messages)) {
+            throw new CommandException($command, 'Empty response', 500);
+        }
+
+        return $messages;
+    }
+
+    /**
+     * @param CommandInterface $command
+     * @return ResponseInterface
