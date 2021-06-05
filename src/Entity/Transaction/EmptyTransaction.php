a868038610e6e44c877c356e67329cbb1b527b9b
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-06-05 07:14:20 +0000

    Use Travis CI

--- src/Entity/Transaction/EmptyTransaction.php
+++ src/Entity/Transaction/EmptyTransaction.php
@@ -3,4 +3,8 @@
 /**
  * Copyright (c) 2018-2021 Adshares sp. z o.o.
  *
- * This file is pa
+ * This file is part of ADS PHP Client
+ *
+ * ADS PHP Client is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Softw
