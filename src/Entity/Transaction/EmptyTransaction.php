a0fceec2d0ad5dba3ec66a2d99c4470f7fb6ff06
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-06-06 07:10:18 +0000

    s390: fix linker script for 31 bit builds

--- src/Entity/Transaction/EmptyTransaction.php
+++ src/Entity/Transaction/EmptyTransaction.php
@@ -12,4 +12,8 @@
  *
  * ADS PHP Client is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty
- * o
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ADS P
