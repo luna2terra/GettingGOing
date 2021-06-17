232a98be6de9b7b98377bfe014e1bc5beb3a816a
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-06-17 09:58:27 +0000

    MIT License

--- src/Entity/Transaction/GetTargetAddressTrait.php
+++ src/Entity/Transaction/GetTargetAddressTrait.php
@@ -16,4 +16,17 @@
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with ADS PHP Client. If not, see <https://www.gnu.
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Entity\Transaction;
+
+use Adshares\Ads\Util\AdsChecksumGenerator;
+
+trait GetTargetAddressTrait
+{
+    /**
+     * @return null|string
+     */
+    public function getTargetAddress(): ?string
+    {
