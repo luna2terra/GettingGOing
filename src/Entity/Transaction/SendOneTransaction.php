22cd0a866aba24bed59261bbde92d3a3522c1459
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-07-01 12:48:05 +0000

    Clarify wording

--- src/Entity/Transaction/SendOneTransaction.php
+++ src/Entity/Transaction/SendOneTransaction.php
@@ -6,4 +6,8 @@
  * This file is part of ADS PHP Client
  *
  * ADS PHP Client is free software: you can redistribute and/or modify it
- * under the terms of the GNU Gen
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ADS PHP Client is distributed in the hope that it will be u
