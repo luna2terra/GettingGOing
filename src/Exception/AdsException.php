d45bc2318a5c31f4165f08718924a3bb5f38306e
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-07-19 07:49:16 +0000

    main.c

--- src/Exception/AdsException.php
+++ src/Exception/AdsException.php
@@ -5,4 +5,9 @@
  *
  * This file is part of ADS PHP Client
  *
- * ADS PHP Client is free software: you can redistr
+ * ADS PHP Client is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ADS PHP Client is distributed in the h
