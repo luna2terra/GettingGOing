dcff91d752089b45ab20567c065a17ab23fb25a1
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-08-02 07:19:32 +0000

    Update changelog. Bump version.

--- src/Response/GetAccountResponse.php
+++ src/Response/GetAccountResponse.php
@@ -15,4 +15,11 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * Y
+ * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Response;
+
+use Adshares\Ads\Entity\Account;
+us
