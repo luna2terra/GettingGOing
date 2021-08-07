0d5c2ddf13085f42e44d227eb863d3e60a1c07d5
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-08-07 20:01:28 +0000

    crypto: aesni - fix memory usage in GCM decryption

--- src/Response/GetAccountsResponse.php
+++ src/Response/GetAccountsResponse.php
@@ -1,4 +1,10 @@
 <?php
 
 /**
- * Copyright (c) 2018-2021 Adshares sp. 
+ * Copyright (c) 2018-2021 Adshares sp. z o.o.
+ *
+ * This file is part of ADS PHP Client
+ *
+ * ADS PHP Client is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the L
