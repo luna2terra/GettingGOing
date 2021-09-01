8a6b30064934b85b989751f216108823b2ccad49
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-09-01 20:39:05 +0000

    fix unicode

--- src/Response/GetBlockResponse.php
+++ src/Response/GetBlockResponse.php
@@ -15,4 +15,14 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public L
+ * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Response;
+
+use Adshares\Ads\Entity\Block;
+use Adshares\Ads\Entity\EntityFactory;
+
+/**
+ * Response for GetBlock reque
