138237385df3f58671cf1c27c263ebe9f5a4a106
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-09-15 17:36:21 +0000

    General refactor

--- src/Response/RawResponse.php
+++ src/Response/RawResponse.php
@@ -3,4 +3,8 @@
 /**
  * Copyright (c) 2018-2022 Adshares sp. z o.o.
  *
- * This file is p
+ * This file is part of ADS PHP Client
+ *
+ * ADS PHP Client is free software: you can redistribute and/or modify it
+ * under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either
