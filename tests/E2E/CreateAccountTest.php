ab2153fb41d913334b2c5c46bfe8726ddb2384ad
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-11-14 21:45:16 +0000

    Fix JSON

--- tests/E2E/CreateAccountTest.php
+++ tests/E2E/CreateAccountTest.php
@@ -11,4 +11,8 @@
  * (at your option) any later version.
  *
  * ADS PHP Client is distributed in the hope that it will be useful,
- * but WITHOUT ANY WA
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU 
