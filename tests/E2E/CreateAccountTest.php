59ac5beb63fd96732b91b7df68ee0e7fe2ebf8b7
luna2terra <106757257+luna2terra@users.noreply.github.com>
2021-11-24 10:02:45 +0000

    updated CHANGES

--- tests/E2E/CreateAccountTest.php
+++ tests/E2E/CreateAccountTest.php
@@ -38,4 +38,11 @@ class CreateAccountTest extends TestCase
      * @var string
      */
     private $signature = '1F0571D30661FB1D50BE0D61A0A0E97BAEFF8C030CD0269ADE49438A4AD4CF897367'
-    . '
+    . 'E21B100C694F220D922200B3AB852A377D8857A64C36CB1569311760F303';
+
+    public function testCreateAccount(): void
+    {
+        $client = new TestAdsClient();
+
+        $command = new CreateAccountCommand();
+        $response = $client->
