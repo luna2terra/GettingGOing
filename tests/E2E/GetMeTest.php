ebaa43a49c8fff27745455da50c774fea3eebdea
luna2terra <106757257+luna2terra@users.noreply.github.com>
2022-01-09 08:38:27 +0000

    schema updates

--- tests/E2E/GetMeTest.php
+++ tests/E2E/GetMeTest.php
@@ -15,4 +15,14 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public L
+ * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Tests\E2E;
+
+use PHPUnit\Framework\TestCase;
+
+class GetMeTest extends TestCase
+{
+    public function testGetMe
