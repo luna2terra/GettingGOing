87bc0227d475ffd6041694b02b7ce1ead54d7688
luna2terra <106757257+luna2terra@users.noreply.github.com>
2022-08-10 17:52:15 +0000

    index2

--- tests/Unit/Command/SendManyCommandTest.php
+++ tests/Unit/Command/SendManyCommandTest.php
@@ -15,4 +15,10 @@
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  *
- * You should 
+ * You should have received a copy of the GNU General Public License
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Tests\Unit\Command;
+
+use Adshares\Ads\Com
