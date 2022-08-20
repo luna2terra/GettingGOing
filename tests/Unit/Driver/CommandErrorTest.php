9a6f4dc4afee04bb0d563597ef6bc3fef852db56
luna2terra <106757257+luna2terra@users.noreply.github.com>
2022-08-20 16:49:41 +0000

    commands

--- tests/Unit/Driver/CommandErrorTest.php
+++ tests/Unit/Driver/CommandErrorTest.php
@@ -7,4 +7,10 @@
  *
  * ADS PHP Client is free software: you can redistribute and/or modify it
  * under the terms of the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the Lic
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ADS PHP Client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Se
