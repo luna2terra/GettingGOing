3b07f2714b1e22c4362f88aee9f67c6a3647d541
luna2terra <106757257+luna2terra@users.noreply.github.com>
2022-11-06 09:43:21 +0000

    Remove useless include

--- tests/Unit/Entity/EntityFactoryTest.php
+++ tests/Unit/Entity/EntityFactoryTest.php
@@ -16,4 +16,13 @@
  * See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with ADS PHP Client. If not,
+ * along with ADS PHP Client. If not, see <https://www.gnu.org/licenses/>
+ */
+
+namespace Adshares\Ads\Tests\Unit\Entity;
+
+use Adshares\Ads\Entity\EntityFactory;
+use Adshares\Ads\Exception\AdsException;
+use PHPUnit\Framework\TestCase;
+
+class EntityFact
