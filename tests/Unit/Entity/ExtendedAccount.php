4a846d6e8c280f29cf83eb39efb2f11995ff59ae
luna2terra <106757257+luna2terra@users.noreply.github.com>
2022-11-08 10:49:19 +0000

    Update gui.py

--- tests/Unit/Entity/ExtendedAccount.php
+++ tests/Unit/Entity/ExtendedAccount.php
@@ -26,4 +26,13 @@ use Adshares\Ads\Entity\Account;
 /**
  * Class ExtendedAccount is extended from Account to perform AdsClient.testSetEntityMap() test.
  *
- * @pa
+ * @package Adshares\Ads\Tests\Unit\Entity
+ */
+class ExtendedAccount extends Account
+{
+    /**
+     * @return string Account address
+     */
+    public function getId(): string
+    {
+        return $this->getAd
