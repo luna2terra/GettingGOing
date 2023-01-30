2240989216f17726493a6b37121d15671a598b2e
luna2terra <106757257+luna2terra@users.noreply.github.com>
2023-01-30 05:40:11 +0000

    Update fr.coffee

--- tests/Unit/Raw.php
+++ tests/Unit/Raw.php
@@ -153,3 +153,236 @@ class Raw
             },
             "account": {
                 "address": "0001-00000000-9B6F",
+                "node": "1",
+                "id": "0",
+                "msid": "4",
+                "time": "1536734271",
+                "date": "2018-09-12 06:37:51",
+                "status": "0",
+                "paired_node": "0",
+                "paired_id": "0",
+                "local_change": "1536734240",
+                "remote_change": "1536734208",
+                "balance": "19999999.99681894976",
+                "public_key": "A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363",
+                "hash": "F88B31EEB3ABEC09D55EC5891DDF3D8A5326DBD6CDFDF687117C0A7581D7260C"
+            }
+        }';
+    }
+
+    public static function getAccount(): string
+    {
+        return '{
+            "current_block_time": "1532415264",
+            "previous_block_time": "1532415232",
+            "tx": {
+                "data": "1001000000000001000000000027CD565B",
+                "signature": "BB196BECB8DD8E6759E7FDFC1A738307F1CE1A052066EE612ADCAC633BE759A685941544B0'
+            . '9BF4EA28185DEB60ABEBB24A4C5170226834ACE5E978C45DD75609",
+                "time": "1532415271"
+            },
+            "account": {
+                "address": "0001-00000000-9B6F",
+                "node": "1",
+                "id": "0",
+                "msid": "3",
+                "time": "1532415270",
+                "date": "2018-07-24 08:54:30",
+                "status": "0",
+                "paired_node": "1",
+                "paired_id": "0",
+                "local_change": "1532415264",
+                "remote_change": "1532415232",
+                "balance": "19999699.84935875759",
+                "public_key": "A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363",
+                "hash": "8592795CE4EE7AAEEC7BA0EBCB4E5B83DF0151B009363FECB99EB39B62549343"
+            },
+            "network_account": {
+                "address": "0001-00000000-9B6F",
+                "node": "1",
+                "id": "0",
+                "msid": "2",
+                "time": "1532415268",
+                "date": "2018-07-24 08:54:28",
+                "status": "0",
+                "paired_node": "0",
+                "paired_id": "0",
+                "local_change": "1532415264",
+                "remote_change": "1532415232",
+                "balance": "19999899.94935875759",
+                "public_key": "A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363",
+                "hash": "3A94A2410419BCE77E40206ADA6E6DFBD55117BC3D137EF4EF3AD9058BADEFF3",
+                "checksum": "true"
+            }
+        }';
+    }
+
+    public static function getAccounts(): string
+    {
+        return '{
+            "current_block_time": "1532422144",
+            "previous_block_time": "1532422112",
+            "tx": {
+                "data": "180100000000000AE8565BE0E7565B0100",
+                "signature": "3F7F802C52449775234D6FCA015D9AEB9E270F1D27BC1A091CCF9659FE52B7C81C8B99E04F46D53F9D8'
+            . '5135F929F3498A7AEEBFEF87FAA3DA91D1E412F94EB06",
+                "time": "1532422154",
+                "account_msid": "0",
+                "account_hashin": "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+                "account_hashout": "E6DE11BBE1A5E7E10E8D10C01C2B5F83DB6AF1C2D0A22A0B38F1F0B5EA034365",
+                "deduct": "0.00000000000",
+                "fee": "0.00000000000"
+            },
+            "accounts": [{
+                    "address": "0001-00000000-9B6F",
+                    "node": "1",
+                    "id": "0",
+                    "msid": "4",
+                    "time": "1532415271",
+                    "date": "2018-07-24 08:54:31",
+                    "status": "0",
+                    "paired_node": "0",
+                    "paired_id": "0",
+                    "local_change": "1532415264",
+                    "remote_change": "1532422080",
+                    "balance": "19999699.87509796942",
+                    "public_key": "A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363",
+                    "hash": "04D526CB20CCE3003B8A2103C5401ABBCAA3F42D03C2392629B6CF923F66323B"
+                }, {
+                    "address": "0001-00000001-8B4E",
+                    "node": "1",
+                    "id": "1",
+                    "msid": "1",
+                    "time": "1532414432",
+                    "date": "2018-07-24 08:40:32",
+                    "status": "0",
+                    "paired_node": "1",
+                    "paired_id": "1",
+                    "local_change": "1532414432",
+                    "remote_change": "1532421984",
+                    "balance": "50199.98821000000",
+                    "public_key": "A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363",
+                    "hash": "4528E7563589895DE3F18F0A28C8EDC159C76EAFE9ACC876A107EBFD534E48D0"
+                }, {
+                    "address": "0001-00000002-BB2D",
+                    "node": "1",
+                    "id": "2",
+                    "msid": "1",
+                    "time": "1532414432",
+                    "date": "2018-07-24 08:40:32",
+                    "status": "0",
+                    "paired_node": "1",
+                    "paired_id": "2",
+                    "local_change": "1532414432",
+                    "remote_change": "1532421984",
+                    "balance": "8305.98820000000",
+                    "public_key": "A9C0D972D8AAB73805EC4A28291E052E3B5FAFE0ADC9D724917054E5E2690363",
+                    "hash": "F3816C5C21B6E7C163894621D8B4FEA0C6053C4CE5F7A4B70516745987AAAF49"
+                }
+            ]
+        }';
+    }
+
+    public static function getBlock(): string
+    {
+        return '{
+            "current_block_time": "1532421440",
+            "previous_block_time": "1532421408",
+            "tx": {
+                "data": "1701000000000020E5565B4DE5565B",
+                "signature": "076872033889584BFB8FBB7C05FDD36604111DCABDF3BC46CC89337D08B0563D521767B'
+            . '61B60467E39371F60A857815E38947D5F9DED98A412F037813FA5E60E",
+                "time": "1532421453"
+            },
+            "block_time_hex": "5B56E520",
+            "block_time": "1532421408",
+            "block_prev": "5B56E500",
+            "block_next": "5B56E540",
+            "block": {
+                "id": "5B56E520",
+                "time": "1532421408",
+                "message_count": "4",
+                "oldhash": "EFAA216F39750FE47ABABA66AB9168C7DBB727F202C2B45A5C85516211BC5DDB",
+                "minhash": "8E466BA9FC2DDDDFA6C70365E59414C00769C77178BF9744AFCDC0AFFFFEE41E",
+                "msghash": "EEF5F90F44C2C2F8B7A8A274FF32CE71F3455BAB4EB6DD97FFB5EBF62B06D006",
+                "nodhash": "A55EF15FA68BECCAAF9CCC11DFDD7DD27A42148B5A76BB003F199578C1DD4E90",
+                "viphash": "2A4831F1459C42E2CCF5C4E202C3301F94C381B6FB253DFED21DD015180D9507",
+                "nowhash": "FD214FC008CDEE51A921CFB133D8A9FF8B503107BEC73B736E0225F6073B033C",
+                "vote_yes": "0",
+                "vote_no": "0",
+                "vote_total": "3",
+                "node_count": "4",
+                "dividend_balance": "0.00000000000",
+                "dividend_pay": "false",
+                "nodes": [{
+                        "id": "0000",
+                        "public_key": "0000000000000000000000000000000000000000000000000000000000000000",
+                        "hash": "0000000000000000000000000000000000000000000000000000000000000000",
+                        "message_hash": "5611343B65A0A3A08C59F6923DDC9CC4BB3B7DE0A12CE4107B503A39B250A280",
+                        "msid": "0",
+                        "mtim": "1532414432",
+                        "balance": "0.00000000000",
+                        "status": "0",
+                        "account_count": "0",
+                        "port": "0",
+                        "ipv4": "0.0.0.0"
+                    }, {
+                        "id": "0001",
+                        "public_key": "73A5C92FA5142599B1C9863B43E026AFEFA6B57AEE8D189241C7F50C90BA5122",
+                        "hash": "6241B452CAA725F45057B8D45A2DF37FC994ADF669F5F7E37976A90BA33E1A2E",
+                        "message_hash": "CF97D2EF6B9A67D39F0F96A934E81B9B9F9E1F3990F8D7246B355387EB0DB775",
+                        "msid": "106",
+                        "mtim": "1532421414",
+                        "balance": "20058205.85390889333",
+                        "status": "6",
+                        "account_count": "3",
+                        "port": "8001",
+                        "ipv4": "172.16.222.101"
+                    }, {
+                        "id": "0002",
+                        "public_key": "FC4CA38301AC2080ADA2BC08C4A94405E546B659BD2EB559C1520A55586336CD",
+                        "hash": "EF89516D2998E1C3646A07A1BB77E5E8C1C97EF973C12FE2F59568738B44CF31",
+                        "message_hash": "943029F6A0ED646174D971278409D998D6853AA5C779736851F575339BDD4B06",
+                        "msid": "611",
+                        "mtim": "1532421420",
+                        "balance": "10699999.98252993070",
+                        "status": "2",
+                        "account_count": "2",
+                        "port": "8002",
+                        "ipv4": "172.16.222.101"
+                    }, {
+                        "id": "0003",
+                        "public_key": "5138AA57FAF5F7F2E67BDCA14F3CC377CD4C681B3F8B5A41DDDD590BD36C3125",
+                        "hash": "BB7BC80A87C4E5A30F8AD318B598D943765E4D03D7406E61F5D8B4B5701B459A",
+                        "message_hash": "15CAAFF4027F8BDB018F989F2753B87DEA2C05E84AA688E379E7864960E79794",
+                        "msid": "98",
+                        "mtim": "1532421414",
+                        "balance": "7999999.99119369209",
+                        "status": "2",
+                        "account_count": "1",
+                        "port": "8003",
+                        "ipv4": "172.16.222.101"
+                    }
+                ]
+            }
+        }';
+    }
+
+    public static function getBlockIds(): string
+    {
+        return '{
+            "current_block_time": "1532421536",
+            "previous_block_time": "1532421504",
+            "tx": {
+                "data": "13010000000000ABE5565B0000000000000000",
+                "signature": "825147B1F0A799D935A15384BC905E3274ACE906FDCB2999713273FB2BAF63CCD28694076EEF23E'
+            . 'E09A7C766950C72C67DB1BF815A484C9372432DD889D6FE0B",
+                "time": "1532421547"
+            },
+            "updated_blocks": "5",
+            "blocks": ["5B56C9E0", "5B56CA00", "5B56CA20", "5B56CA40", "5B56CA60"]
+        }';
+    }
+
+    public static function getBroadcast(): string
+    {
